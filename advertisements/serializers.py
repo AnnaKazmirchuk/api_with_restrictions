50ea029ac3868f06f2aee18aee9c94bae6df3fac
Anna Kazmirchuk <anyuta.kazmirchuk32@gmail.com>
2022-05-26 09:26:54 +0300

    Amended 2

--- advertisements/serializers.py
+++ advertisements/serializers.py
@@ -44,15 +44,9 @@ class AdvertisementSerializer(serializers.ModelSerializer):
 
         request = self.context['request']
 
-        if data.get('status') == 'OPEN':
+        if request.method == 'POST' or data.get('status') == 'OPEN':
 
             if Advertisement.objects.filter(status='OPEN').count() > 10:
                 raise ValidationError('You have more than 10 open ads')
-
-            if request.method == 'POST':
-                if data['status'] == 'OPEN':
-
-                    if Advertisement.objects.filter(status='OPEN').count() > 10:
-                        raise ValidationError('You have more than 10 open ads')
 
         return data
